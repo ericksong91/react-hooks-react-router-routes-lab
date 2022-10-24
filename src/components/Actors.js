bc32db16831ba0a0bddf9fed8fb4c709871893d0
Eric Song <ericksong91@gmail.com>
2022-10-24 17:57:36 -0400

    commit tree fb6d670ec1ba9799042fac55882db8c6c7fd2db5

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,30 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  console.log(actors)
+
+  const actorsList = actors.map((act, ind)=>{
+    const movieList = act.movies.map((mov, index) => {
+      return <li key={`Movie-${index}`}>{mov}</li>
+    })
+    return (
+      <div key={`Actors-${ind}`}>
+        <h2>Name: {act.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {movieList}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actorsList}
+  </div>
+  )
 }
 
 export default Actors;
