4a19d1d14bc27a7a81368e479e21982af2a6776e
Eric Song <ericksong91@gmail.com>
2022-10-24 17:57:44 -0400

    done with assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,30 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  console.log(directors)
+
+  const directorsList = directors.map((dir, ind) => {
+    const movieList = dir.movies.map((mov, index) => {
+      return <li key={`Movie-${index}`}>{mov}</li>
+    })
+    return (
+      <div key={`Director-${ind}`}>
+        <h2>Name: {dir.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {movieList}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsList}
+    </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((mov, ind)=>{
+    const genreList = mov.genres.map((gen, index)=>{
+      return <li key={`Genre-${index}`}>{gen}</li>
+    })
+
+    return (<div key={`Movie-${ind}`} >
+      <h2>Name: {mov.title}</h2>
+      <p>Time: {mov.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {genreList}
+      </ul>
+    </div>)
+  })
+
+  return (
+    <div>
+          <h1>Movies Page</h1>
+          {movieList}
+    </div>
+
+
+  )
 }
 
 export default Movies;
